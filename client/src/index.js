6bcc947e232d89c5ab3ebd7cd1da481990e82e4b
Julien Sebag <julien.sebag@me.com>
2022-02-14 12:30:44 +0100

    Put back test files

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 
+// Tests
+import reportWebVitals from "./tests/reportWebVitals"
+
 // Components
 import { AuthProviderWrapper } from "./context/auth"
 import App from "./App"
@@ -19,3 +22,8 @@ ReactDOM.render(
     </AuthProviderWrapper>,
     document.getElementById("root")
 )
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
